f8f01c6710c643432e2d11fb58e92397f8ec405e
brandon <bknox.dev@gmail.com>
2020-07-26 05:54:03 -0700

    Added navigation elements

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Brandon Knox on 7/26/20.
+//  Copyright © 2020 Brandon Knox. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
