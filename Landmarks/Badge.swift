baa4277a4939cec36df9b37ba435271174ac7ed7
brandon <bknox.dev@gmail.com>
2020-07-27 21:30:36 -0700

    started badge file

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Brandon Knox on 7/27/20.
+//  Copyright © 2020 Brandon Knox. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
