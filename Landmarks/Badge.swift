3bbef51d1ab384517ae7b4ec128447ec42d4e33f
brandon <bknox.dev@gmail.com>
2020-07-28 05:18:45 -0700

    tweaking drawing of badge hexagon

--- Landmarks/Badge.swift
+++ Landmarks/Badge.swift
@@ -10,21 +10,29 @@ import SwiftUI
 
 struct Badge: View {
     var body: some View {
-        Path { path in
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(to: CGPoint(x: width * 0.95, y: height * 0.20))
-            
-            HexagonParameters.points.forEach {
-                path.addLine(
-                    to: .init(
-                        x: width * $0.useWidth.0 * $0.xFactors.0,
-                        y: height * $0.useHeight.0 * $0.yFactors.0
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = 100.0
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95,
+                        y: height * 0.20 + HexagonParameters.adjustment))
+                
+                HexagonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: width * $0.useWidth.0 * $0.xFactors.0,
+                            y: height * $0.useHeight.0 * $0.yFactors.0
+                        )
                     )
-                )
-            }
+                }
+            }.fill(LinearGradient(gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]), startPoint: .init(x: 0.5, y: 0), endPoint: .init(x: 0.5, y: 0.6)))
+
         }
-        .fill(Color.black)
     }
 }
 
