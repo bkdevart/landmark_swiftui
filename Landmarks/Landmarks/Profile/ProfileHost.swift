3480f4d987287d7fa12b0de83c0fb75ac188516f
Brandon Knox <bknox.dev@gmail.com>
2020-11-22 11:44:23 -0700

    Updated view

--- Landmarks/Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -9,8 +9,10 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @State var draftProfile = Profile.default
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Profile for: \(draftProfile.username)")
     }
 }
 
