6609c5edeca370ee1d7cc63804923995d63f38e3
Brandon Knox <bknox.dev@gmail.com>
2020-11-22 11:41:16 -0700

    Created ProfileHost view

---
+++ Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Brandon Knox on 11/22/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
