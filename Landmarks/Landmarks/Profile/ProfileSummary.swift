09356472b1c90409c2a5b924ac7c38eb1d026fb9
Brandon Knox <bknox.dev@gmail.com>
2020-11-22 12:14:25 -0700

    Continuing to test git/xcode

---
+++ Landmarks/Landmarks/Profile/ProfileSummary.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Brandon Knox on 11/22/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary()
+    }
+}
